b66b67ef302ec0f0e1b1c49ab20a786473637751
francies <quebert88@gmail.com>
2023-08-08 21:35:34 +0100

    change X-Robots-Tag to index

--- next.config.js
+++ next.config.js
@@ -3,17 +3,15 @@ const nextConfig = {
     output: 'standalone',
     async headers() {
         const headers = [];
-        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
-            headers.push({
-                headers: [
-                    {
-                        key: 'X-Robots-Tag',
-                        value: 'noindex',
-                    },
-                ],
-                source: '/:path*',
-            });
-        }
+        headers.push({
+            headers: [
+                {
+                    key: 'X-Robots-Tag',
+                    value: 'index',
+                },
+            ],
+            source: '/:path*',
+        });
         return headers;
     },
 }
